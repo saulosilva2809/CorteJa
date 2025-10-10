32dba2332777fcaed1ad1d83e7cd42a2eaf61d4f
saulosilva2809 <saulosilva2809@users.noreply.github.com>
2025-10-09 22:03:31 -0300

    fix: fix the templates directory

--- src/core/settings.py
+++ src/core/settings.py
@@ -48,7 +48,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR.parent, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
