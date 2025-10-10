cbcdd7c321d1efd4d1d94146fcdbc519171c6b54
saulosilva2809 <saulosilva2809@users.noreply.github.com>
2025-10-09 21:40:30 -0300

    feat: create BaseModel to be used as inheritance in other models

---
+++ src/apps/base/models/base_model.py
@@ -0,0 +1,12 @@
+import uuid
+
+from django.db import models
+
+
+class BaseModel(models.Model):
+    pk = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
