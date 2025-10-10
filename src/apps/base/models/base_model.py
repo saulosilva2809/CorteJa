a716402f5957834877a9a6e7d284795517a3adae
saulosilva2809 <saulosilva2809@users.noreply.github.com>
2025-10-09 22:11:33 -0300

    fix: rename pk field to id

--- src/apps/base/models/base_model.py
+++ src/apps/base/models/base_model.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class BaseModel(models.Model):
-    pk = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
