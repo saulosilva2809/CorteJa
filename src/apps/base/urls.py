11e1aa68fd6ffb7025c7af3019991b3ed8a73fb7
saulosilva2809 <saulosilva2809@users.noreply.github.com>
2025-10-11 00:18:01 -0300

    feat and style: created new feature contact

--- src/apps/base/urls.py
+++ src/apps/base/urls.py
@@ -1,13 +1,15 @@
 from .views import (
+    ContactView,
     HomeView,
     ProfessionalsView,
-    ServicesView
+    ServicesView,
 )
 
 from django.urls import path
 
 
 urlpatterns = [
+    path('contact/', view=ContactView.as_view(), name='contact_view'),
     path('', view=HomeView.as_view(), name='home_view'),
     path('professionals/', view=ProfessionalsView.as_view(), name='professionals_view'),
     path('services/', view=ServicesView.as_view(), name='services_view'),
--- src/apps/base/views/__init__.py
+++ src/apps/base/views/__init__.py
@@ -1,3 +1,4 @@
+from .contact_view import ContactView
 from .home_view import HomeView
 from .professionals_view import ProfessionalsView
 from .services_view import ServicesView
