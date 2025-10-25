a8a7fc08350931fa79f007b21cba611582ae01da
saulosilva2809 <saulosilva2809@users.noreply.github.com>
2025-10-25 09:02:01 -0300

    remove: remove arquivos que não serão usados

--- src/apps/base/tasks.py
+++
@@ -1,7 +0,0 @@
-from celery import shared_task
-
-
-@shared_task
-def ping_task(name: str = "mundo"):
-    """Tarefa simples para testar execução do Celery."""
-    return f"Olá, {name}! Celery está funcionando."
